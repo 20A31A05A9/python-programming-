390354a77757643d9c0bbb8224fdd450013f8980
chaitanya reddy <109856718+20A31A05A9@users.noreply.github.com>
2023-02-01 15:31:27 +0530

    Add files via upload

---
+++ day3 programming.py
@@ -0,0 +1,28 @@
+#conversion into binary
+b=(6>>+2)
+print(bin(b))
+#swapping by addition and substraction
+a=10
+b=20
+a=a+b
+b=a-b
+a=a-b
+print(a,b)
+#swapping by product and multiplication
+a=11
+b=22
+a=a*b
+b=a//b
+a=a//b
+print(a,b)
+#cirumflex
+a=5
+b=6
+a=a^b
+b=a^b
+a=a^b
+print(a,b)
+
+
+
+
